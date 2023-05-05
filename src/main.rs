0a78fc105a732942274816c69ba7c8882e779bb4
heliannuuthus <heliannuuthus@gmail.com>
2023-05-05 23:38:15 +0800

    feat: websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use controllers::websocket::{connected, count};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+mod controllers;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .with_max_level(Level::INFO)
+            .finish(),
+    )
+    .expect("tracing setting default failed");
+    HttpServer::new(move || {
+        App::new()
+            .route("/ws", web::get().to(connected))
+            .service(count)
+    })
+    .bind(("0.0.0.0", 10090))?
+    .run()
+    .await
 }
